9164f032c50c97fe35a5b534ec947600f7cc1689
Bojana Skarich <bojana.skarich@gmail.com>
2015-04-10 11:44:45 -0700

    Add code to make fourth test pass, which returns an output of scalene type if none of the sides are equal in length.

--- js/scripts.js
+++ js/scripts.js
@@ -11,20 +11,18 @@ var type_of_triangle = "";
         type_of_triangle = 'not a triangle. third side cannot be larger than the sum of two other sides';
 
     //two sides equal to each other and third side is less than their sum
-} else if ((side1 === side2 && side3 < side1 + side2) || (side2 === side3 && side1 < side2 + side3) || (side3 === side1 && side2 < side3 + side1))
+    } else if ((side1 === side2 && side3 < side1 + side2) || (side2 === side3 && side1 <    side2 + side3) || (side3 === side1 && side2 < side3 + side1)) {
         type_of_triangle = 'isosceles';
 
+    } else if (side1 !== side2 !== side3) {
+        type_of_triangle = 'scalene';
 
-
+    }
 
     // if (side1 === side2 === side3) {
     //     type_of_triangle = "Equilateral";
 
-    // } else if (side1 !== side2 !== side3) {
-    //     type_of_triangle = "Scalene";
 
-    // } else if (side1 === side2) && (side2 !== side3) ||
-    // (side2 === side)
 
 
 
