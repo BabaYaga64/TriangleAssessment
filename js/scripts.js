a045b84f37b663ae8cf90b492671bad4e4a58d5a
Bojana Skarich <bojana.skarich@gmail.com>
2015-04-13 20:53:39 -0700

    Change if else statements to test for if it is a triangle at all first, then type of triangle. Modify expected input values so that all tests pass.

--- js/scripts.js
+++ js/scripts.js
@@ -1,48 +1,21 @@
 var triangleType = function(side1, side2, side3) {
 
-var type_of_triangle = "";
-
-    //all sides equal to zero
-    if (side1 === 0 || side2 === 0 || side3 === 0) {
-        type_of_triangle = 'this is not a triangle';
-
-    //third side larger than or equal to the sum of other two sides
-    } else if ((side1 >= (side2 + side3)) || (side2 >= (side1 + side3)) || (side3 >= (side1     + side2))) {
-        type_of_triangle = 'not a triangle, third side too long';
-
-    //ISOSCELES: two sides equal to each other and third side is less than their sum
-    //side 1 not equal to side 3
-    } else if ((side1 === side2) && (side3 < side1 + side2) && (side1 !== side3)) {
-        type_of_triangle = 'isosceles';
-    //side 1 not equal to side 2
-    } else if (side2 === side3 && side1 < side2 + side3 && side1 !== side2) {
-        type_of_triangle = 'isosceles';
-    //side 2 not equal to side 3
-    } else if (side3 === side1 && side2 < side3 + side1 && side2 !== side3) {
-        type_of_triangle = 'isosceles';
-
-    //SCALENE: no sides equal to each other
-    //side1 less than sum of side2 and side3
-    } else if (side1 !== side2 !== side3 && (side1 < side2 + side3)) {
-        type_of_triangle = 'scalene';
-     //side2 less than sum of side1 and side3
-    } else if (side1 !== side2 !== side3 && (side2 < side1 + side3)) {
-        type_of_triangle = 'scalene';
-     //side3 less than sum of side1 and side2
-    } else if (side1 !== side2 !== side3 && (side3 < side1 + side2)) {
-        type_of_triangle = 'scalene';
+   //checks to see if it is a triangle at all
+   if (side1 >= (side2 + side3) || (side2 >= (side1 + side3)) || (side3 >= (side1 + side2))) {
+        return 'not a triangle';
 
     //EQUILATERAL: all sides equal to each other
-    //side1 less than sum of side2 and side3
-    } else if (side1 === side2 === side3 && side1 < side2 + side3
-    //side2 less than sum of side1 and side3
-    || (side1 === side2 === side3 && side2 < side1 + side3)
-    //side3 less than sum of side1 and side2
-    || (side1 === side2 === side3 && side3 < side1 + side2)) {
-        type_of_triangle = 'equilateral';
-    }
+    } else if (side1 === side2 && side2 === side3) {
+        return 'equilateral';
 
-    return type_of_triangle;
+    //ISOSCELES: two sides equal to each other
+    } else if ((side1 === side2 && side2 !== side3) || (side2 === side3 && side3 !== side1)|| (side3 === side1 && side1 !== side2)) {
+        return 'isosceles';
+
+    //SCALENE: no sides equal to each other
+    } else if (side1 !== side2 && side2 !== side3) {
+         return 'scalene';
+    }
 
 };
 
@@ -57,9 +30,10 @@ $(document).ready(function() {
         $(".side2").text(side2);
         $(".side3").text(side3);
 
-
+        $(".result").text(result);
 
         $("#result").show();
+
         event.preventDefault();
     });
 });
--- spec/specs.js
+++ spec/specs.js
@@ -1,38 +1,28 @@
-describe("triangleType", function() {
-    //input: all sides equal to zero
-    //output: all sides must be larger than zero
-    it("returns an output of 'this is not a triangle' if the input is 0, 0, 0", function() {
-    expect(triangleType(0, 0, 0)).to.equal('this is not a triangle');
-    });
-
-    //input: one side equal to zero
-    //output: all sides must be larger than zero
-    // it("returns an output of 'all sides must be larger than zero' if any one of the inputs is 0", function() {
-    //     expect(triangleType(0, 1, 1)).to.equal('all sides must be larger than zero');
-    // });
+describe('triangleType', function() {
 
     //input: third side larger than or equal to the sum of other two sides
-    //output: this is not a triangle
-    it("returns an output of 'not a triangle, third side too long' if the third side is larger than or equal to the sum of the other two sides", function() {
-        expect(triangleType(7, 2, 3)).to.equal('not a triangle, third side too long');
-    });
-
-    //input: two sides equal to each other and third side is less than their sum
-    //output: isosceles
-    it("returns an output of 'isosceles' if two sides are equal to each other and the third side is less than their sum", function() {
-        expect(triangleType(7, 7, 2)).to.equal('isosceles');
-    });
-
-    //input: no sides equal to each other
-    //output: scalene
-    it("returns an output of 'scalene' if none of the sides are equal to each other", function() {
-        expect(triangleType(2, 3, 5)).to.equal('scalene');
+    //output: not a triangle
+    it("returns an output of 'not a triangle' if the third side is larger than or equal to the sum of the other two sides", function() {
+        expect(triangleType(7, 2, 3)).to.equal('not a triangle');
     });
 
     //input: all sides equal to each other
     //output: equilateral
     it("returns an output of 'equilateral' if all of the sides are equal", function() {
         expect(triangleType(2, 2, 2)).to.equal('equilateral');
+    });
+
+
+    //input: two sides equal to each other and third side is less than their sum
+    //output: isosceles
+    it("returns an output of 'isosceles' if two sides are equal to each other and the third side not equal to either of them", function() {
+        expect(triangleType(7, 7, 2)).to.equal('isosceles');
+    });
+
+    //input: no sides equal to each other
+    //output: scalene
+    it("returns an output of 'scalene' if none of the sides are equal to each other", function() {
+        expect(triangleType(2, 3, 4)).to.equal('scalene');
     });
 
 });
